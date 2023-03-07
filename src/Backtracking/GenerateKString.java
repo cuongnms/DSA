e7615f5a5825029958a1910d822df5e61b38ebb0
CuongNMs <nguyen.minh.cuong.1102@gmail.com>
2023-03-07 23:26:08 +0700

    add backtracking algo

--- src/Backtracking/GenerateKString.java
+++ src/Backtracking/GenerateKString.java
@@ -1,9 +1,25 @@
 package Backtracking;
 
-public class GenerateKString {
+import java.util.Arrays;
 
+public class GenerateKString {
+    private int count = 0;
     public static void main(String[] args) {
         GenerateKString g = new GenerateKString();
-        a
+        g.generateKString(10000, 10,  new int[10000]);
+        System.out.println(g.count);
+    }
+
+    private void generateKString(int i,int k, int[] ints) {
+        if(i<1){
+            System.out.println(Arrays.toString(ints));
+            count++;
+            return;
+        }else{
+            for(int j = 0; j < k; j++){
+                ints[i-1] = j;
+                generateKString(i-1, k, ints);
+            }
+        }
     }
 }
