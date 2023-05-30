898f22a1e141d380b7b90afb085dce6803e9d544
CuongNMs <nguyen.minh.cuong.1102@gmail.com>
2023-05-30 13:04:03 +0700

    update logic dailytemp prob

--- src/stack/DailyTemp.java
+++ src/stack/DailyTemp.java
@@ -1,4 +1,27 @@
 package stack;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class DailyTemp {
+    /*
+    * Create stack for save index of temperature that have temperature lower than current temp in loop.
+    * */
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+        for(int i = 0 ; i < temperatures.length; i++){
+            while(!stack.isEmpty() && temperatures[i]> temperatures[stack.peek()]){
+                int indexOfResult = stack.pop();
+                result[indexOfResult] = i - indexOfResult;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        DailyTemp d = new DailyTemp();
+        System.out.println(Arrays.toString(d.dailyTemperatures(new int[]{73,74,75,71,69,72,76,73})));
+    }
 }
